a84e4fc7e373367591e687da0da7e1e54b28ef3a
KonstantIMP <mihedovkos@gmail.com>
2020-11-20 17:08:13 +0300

    Done Color.d comments

--- source/Color.d
+++ source/Color.d
@@ -1,12 +1,20 @@
+/// @file   Color.d
+/// 
+/// @brief  RgbaColor struct description
+///
+/// @author KonstantIMP
+/// @date   2020
+///
 module Color;
 
+/// @brief  RgbaColor   Struct to describe pixel color using RGBA color space
 struct RgbaColor {
-
+    /// @brief  r   Amount of red chanel in color
     double r;
-
+    /// @brief  g   Amount of green chanel in color
     double g;
-
+    /// @brief  b   Amount of blue chanel in color
     double b;
-
+    /// @brief  a   Amount of alpha chanel in color
     double a;
 }

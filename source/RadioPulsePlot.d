b9807b1b44512236f45f6fc0b7bd677d9734ade8
KonstantIMP <mihedovkos@gmail.com>
2020-11-25 23:00:08 +0300

    Commentaries were written

--- source/RadioPulsePlot.d
+++ source/RadioPulsePlot.d
@@ -1,11 +1,9 @@
-/**
- * @defgroup   RADIOPULSEPLOT Radio Pulse Plot
- *
- * @brief      This file implements RadioPulsePlot widget.
- *
- * @author     KonstantIMP
- * @date       2020
- */
+/// @file   RadioPulsePlot.d
+/// 
+/// @brief  RadioPulsePlot class description
+///
+/// @author KonstantIMP
+/// @date   2020
 module RadioPulsePlot;
 
 import cairo.c.types;
@@ -26,17 +24,34 @@ import std.conv;
 
 import Color;
 
+/// @brief Enumeration of supported modulation modes for plot drawing
 enum ModeType {
-    frecuency_mode,
-    amplitude_mode,
-    phase_mode
+    frecuency_mode, ///> Frequency modulation mode
+    amplitude_mode, ///> Aplitude modulation mode
+    phase_mode      ///> Phase modulation mode
 }
 
+/// @brief  RadioPulsePlot  Class for drawing radio pulse plot
+///
+/// Plot for viewing bit sequence at radio channel
+/// It is a composite widget
+///
+/// GtkOverlay                  # Base widget
+/// |__ Child
+/// |   |__ GtkScrolledWindow   # For plot scaling
+/// |       |__ GtkDrawingArea  # For plot drawing
+/// |__ Overlay
+///     |__ GtkLabel            # For plot name drawing
 class RadioPulsePlot : Overlay {
+    /// @brief plot_name    Label for plot name drawing
     private Label plot_name;
+    /// @brief plot_area    DrawingArea for plot drawing(Axes and line)
     private DrawingArea plot_area;
+    /// @brief plot_sw      ScrolledWindow for plot scaling
     private ScrolledWindow plot_sw;
 
+    /// @brief  Basic constructor for widget
+    /// Init child widgets, build ui and connect signals
     public this() @trusted { super();  
         plot_name = new Label("");
         plot_area = new DrawingArea();
@@ -46,6 +61,7 @@ class RadioPulsePlot : Overlay {
         resetPlot(); plot_area.addOnDraw(&onDraw);
     }
 
+    /// @brief resetPlot    Set plot attributes at default values
     public void resetPlot() @safe {
         f_width = 20; mod_type = ModeType.phase_mode; 
         bit_sequence = ""; freq = 100; time_discrete = 0.2;
@@ -54,26 +70,40 @@ class RadioPulsePlot : Overlay {
         background_color = RgbaColor (1.0, 1.0, 1.0, 1.0);
     }
 
+    /// @brief drawRequest  Request plot area redraw at GTK
     public void drawRequest() @trusted {
+        /// Set plot area size as zeroes for smart plot scale
         plot_area.setSizeRequest(0, 0);
         plot_area.queueDraw();
     }
 
+    /// @brief createUI Setting correct ui struct
     private void createUI() @trusted {
+        /// Adding plot_sw to Overlay(child)
         add(cast(Widget)(plot_sw));
-
+        /// Adding plot_area to ScrolledWindow(child)
         plot_sw.add(cast(Widget)(plot_area));
 
+        /// Plot name uses markup for work with text and background colors
         plot_name.setUseMarkup(true);
         plot_name.setMarkup("<span size='small' foreground='#000000' background='#ffffff'>График радиосигнала</span>");
 
+        /// Setting plot_name as OverlayWidget
         addOverlay(cast(Widget)plot_name);
 
+        /// Setting plot_name position
         plot_name.setProperty("margin", 5);
         plot_name.setProperty("halign", GtkAlign.END);
         plot_name.setProperty("valign", GtkAlign.START); 
     }
 
+    /// @brief onDraw   Plot drawing slot
+    /// This slot is called every time plot redraw
+    ///
+    /// @param[in]  _context    Cairo context for actually draw
+    /// @param[in]  _widget     Widget that contains cairo surface for drawing
+    ///
+    /// @return     bool        True if drawing was succesfull
     protected bool onDraw(Scoped!Context _context, Widget _widget) {
         GtkAllocation _w_alloc; ulong _actual_size;
 
@@ -92,6 +122,12 @@ class RadioPulsePlot : Overlay {
         return true;
     }
 
+    /// @brief sizeAllocate A function that calculates the size of the plot
+    ///                             based on the number of bits in the sequence
+    ///
+    /// @param[in]  w           Widget to be resized
+    /// @param[out] w_alloc     Calculated widget size
+    /// @param[out] actual_size Calculated distance between unit lines
     protected void sizeAllocate(ref Widget w, out GtkAllocation w_alloc, out ulong actual_size) @trusted {
         w.setSizeRequest(0, 0);
         
@@ -105,6 +141,9 @@ class RadioPulsePlot : Overlay {
         } w.getAllocation(w_alloc);   
     }
 
+    /// @brief drawBackground   Draw plot background
+    ///
+    /// @param[in] cairo_context    Cairo context for drawing
     protected void drawBackground(ref Scoped!Context cairo_context) @trusted {
         cairo_context.setSourceRgba(background_color.r,
                                     background_color.g,
@@ -113,6 +152,10 @@ class RadioPulsePlot : Overlay {
         cairo_context.paint();
     }
 
+    /// @brief drawAxes Draw plot axes
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
     protected void drawAxes(ref Scoped!Context cairo_context, GtkAllocation w_alloc) @trusted {
         cairo_context.setLineWidth(2);
         cairo_context.setSourceRgba(axes_color.r,
@@ -124,6 +167,10 @@ class RadioPulsePlot : Overlay {
         drawYAxis(cairo_context, w_alloc);
     }
 
+    /// @brief drawXAxis Draw X plot axis
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
     protected void drawXAxis(ref Scoped!Context cairo_context, GtkAllocation w_alloc) @trusted {
         cairo_context.moveTo(10, w_alloc.height / 2);
         cairo_context.relLineTo(w_alloc.width - 20, 0);
@@ -132,6 +179,10 @@ class RadioPulsePlot : Overlay {
         cairo_context.stroke();
     }
 
+    /// @brief drawYAxis Draw Y plot axis
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
     protected void drawYAxis(ref Scoped!Context cairo_context, GtkAllocation w_alloc) @trusted {
         cairo_context.moveTo(20, w_alloc.height - 10);     cairo_context.lineTo(20, 10);
         cairo_context.relLineTo(2, 5);      cairo_context.relLineTo(-2, -5);
@@ -139,11 +190,21 @@ class RadioPulsePlot : Overlay {
         cairo_context.stroke();
     }
 
+    /// @brief makeAxesMarkup Draw plot axes markup
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void makeAxesMarkup(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         makeXAxisMarkup(cairo_context, w_alloc, actual_size);
         makeYAxisMarkup(cairo_context, w_alloc);
     }
 
+    /// @brief makeXAxisMarkup Draw X plot axis markup
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void makeXAxisMarkup(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         cairo_context.moveTo(20 + actual_size, w_alloc.height / 2 - 4);
         cairo_context.relLineTo(0, 8);
@@ -155,6 +216,10 @@ class RadioPulsePlot : Overlay {
         }  cairo_context.stroke();
     }
 
+    /// @brief makeYAxisMarkup Draw Y plot axis markup
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
     protected void makeYAxisMarkup(ref Scoped!Context cairo_context, GtkAllocation w_alloc) @trusted {
         /// Drawing +1V value
         cairo_context.moveTo(16, w_alloc.height / 6);
@@ -164,6 +229,11 @@ class RadioPulsePlot : Overlay {
         cairo_context.relLineTo(8, 0); cairo_context.stroke();
     }
 
+    /// @brief makeInscriptions Draw plot inscriprion
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void makeInscriptions(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         textYAxisName(cairo_context, w_alloc);
         textYAxisMarkup(cairo_context, w_alloc);
@@ -171,12 +241,20 @@ class RadioPulsePlot : Overlay {
         textXAxisMarkup(cairo_context, w_alloc, actual_size);
     }
 
+    /// @brief textYAxisName Draw Y plot axis name
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
     protected void textYAxisName(ref Scoped!Context cairo_context, GtkAllocation w_alloc) @trusted {
         cairo_context.setFontSize(10);
         cairo_context.moveTo(5, 10); 
         cairo_context.showText("А");
     }
 
+    /// @brief makeYAxisMarkup Draw Y plot axis markup
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
     protected void textYAxisMarkup(ref Scoped!Context cairo_context, GtkAllocation w_alloc) @trusted {
         /// Y 1V value
         cairo_context.moveTo(5, w_alloc.height / 6 + 3);
@@ -186,12 +264,22 @@ class RadioPulsePlot : Overlay {
         cairo_context.showText("-1");
     }
 
+    /// @brief textXAxisName Draw X plot axis name
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void textXAxisName(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         cairo_context.setFontSize(10);
         cairo_context.moveTo(w_alloc.width - 35, w_alloc.height / 2 + 12);
         cairo_context.showText("t(сек.)");
     }
 
+    /// @brief makeXAxisMarkup Draw X plot axis markup
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void textXAxisMarkup(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         cairo_context.rotate(3.1415 / 2);
         
@@ -212,6 +300,11 @@ class RadioPulsePlot : Overlay {
         cairo_context.rotate(-(3.1415 / 2));
     }
 
+    /// @brief drawPlotLine Draw plot line
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void drawPlotLine(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         cairo_context.setLineWidth(1);
         cairo_context.moveTo(20, w_alloc.height / 2);
@@ -227,6 +320,12 @@ class RadioPulsePlot : Overlay {
         }
     }
 
+    /// @brief drawPhasePlotLine Draw plot line for phase modulation mode
+    /// Phase changes every time signal changes
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void drawPhasePlotLine(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         bool last_state = true; uint need_draw;
         need_draw = cast(uint)(round(time_discrete * freq));
@@ -263,6 +362,12 @@ class RadioPulsePlot : Overlay {
         }
     }
 
+    /// @brief drawAplitudePlotLine Draw plot line for amplitude modulation mode
+    /// Amplitude becames less Bit is in zero value
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void drawAmplitudePlotLine(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         bool last_state = true; uint need_draw;
         need_draw = cast(uint)(round(time_discrete * freq));
@@ -301,6 +406,12 @@ class RadioPulsePlot : Overlay {
         }
     }
 
+    /// @brief drawFrequencyPlotLine Draw plot line for frequency modulation mode
+    /// Frequency becomes less every time Bit is in zero value
+    ///
+    /// @param[in] cairo_context   Cairo context for drawing
+    /// @param[in] w_alloc          Plot area size
+    /// @param[in] actual_size      Distance between unit lines
     protected void drawFrequencyPlotLine(ref Scoped!Context cairo_context, GtkAllocation w_alloc, ulong actual_size) @trusted {
         bool last_state = true; uint need_draw;
         need_draw = cast(uint)(round(time_discrete * freq));
@@ -335,24 +446,39 @@ class RadioPulsePlot : Overlay {
         }
     }
 
+    /// @brief  Distance between units
     private ubyte f_width;
+    /// @brief  fWidth  Getter for f_width
     @property ubyte fWidth() { return f_width; }
+    /// @brief  fWidth  Setter for f_width
     @property ubyte fWidth(ubyte f) { return f_width = f; }
 
+    /// @brief Bit sequence for displaing
     private string bit_sequence; 
+    /// @brief bitSequence    Getter for bit_sequence
     @property string bitSequence() { return bit_sequence; }
+    /// @brief bitSequence    Setter for bit_sequence
     @property string bitSequence(string bits) { return bit_sequence = bits; }
 
+    /// @brief Frequency for considering plot
     private ulong freq;
+    /// @brief  frequency   Getter for freq
     @property ulong frequency() { return freq; }
+    /// @brief  frequency   Setter for freq
     @property ulong frequency(ulong fr) { return freq = fr; } 
 
+    /// @brief Time discrete for X axis
     private double time_discrete;
+    /// @brief timeDiscrete    Getter for time_discrete
     @property double timeDiscrete() { return time_discrete; }
+    /// @brief timeDiscrete    Setter for time_discrete
     @property double timeDiscrete(double dis) { return time_discrete = dis; } 
 
+    /// @brief  Modulation type for plot drawing
     private ModeType mod_type;
+    /// @brief  modeType    Getter for mod_type
     @property ModeType modeType() { return mod_type; }
+    /// @brief  modeType    Setter for mod_type
     @property ModeType modeType(ModeType mt) { return mod_type = mt; }
 
     /// @brief Plot line color

0ecd1484b7e1840cb5ab0513297be2ce9e471c75
KonstantIMP <mihedovkos@gmail.com>
2021-01-07 15:25:22 +0300

    Added plot properties

--- source/NoiseRadioPulsePlot.d
+++ source/NoiseRadioPulsePlot.d
@@ -1,9 +1,18 @@
 module NoiseRadioPulsePlot;
 
 import RadioPulsePlot;
+import Noise;
 
 class NoiseRadioPulsePlot : RadioPulsePlot {
     public this() { super();
         plotName("Полученный сигнал");
     }
+
+    private ulong noise_power;
+    @property public ulong noisePower() @safe @nogc { return noise_power; }
+    @property public ulong noisePower(ulong pow) @safe @nogc { return noise_power = pow; }
+
+    private AWGNoise plot_noise;
+    @property public AWGNoise plotNoise() { return plot_noise; }
+    @property public AWGNoise plotNoise(AWGNoise n) { return plot_noise = n; }
 }
